138c08980539771d7e3f84eacb67648d856eae44
Akami-Usagi <akami.3ds@gmail.com>
2023-02-27 17:06:01 -0500

    creacion de nuevas tareas

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const inputText = document.querySelector("[data-form-input__text]");
-    console.log(inputText.value);
+    const value = inputText.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li");
+    task.classList.add("card")
+    inputText.value = "";
+    //backticks alt+96 
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-edit editIcon icon"></i>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML = content;
+  list.appendChild(task)
+    console.log(content);
 }
 
 
